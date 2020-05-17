b27530116590540844ceee80983f9d49c946b708
markgmz92 <markgmz92@gmail.com>
2020-05-18 00:26:53 +0800

    created Artist scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_162333) do
+ActiveRecord::Schema.define(version: 2020_05_17_162607) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_05_17_162333) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "artists", "users"
 end
